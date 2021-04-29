94bab37f9e9c3828d0da0df9b8ec2d4f6e6bb831
Oleksandr Stepanenko <sashastepanenko2007@gmail.com>
2021-04-29 19:24:50 +0300

    add loaders

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
-import { getRandomJoke } from './fixtures';
 import style from './style.css';
+import generalLoadIcon from './load_general.gif';
+import progLoadIcon from './load_prog.gif';
 
 if (module.hot) {
   module.hot.accept();
@@ -13,11 +14,13 @@ window.dataStore = {
   jokesShown: 0,
   jokesType: 'general',
   shownIds: [],
+  error: null,
+  isDataLoading: false,
 };
 
 window.renderApp = renderApp;
-window.showPunchLine = showPunchLine;
-window.showRandomJoke = showRandomJoke;
+window.setJokePunchLine = setJokePunchLine;
+window.setNewRandomJoke = setNewRandomJoke;
 window.changeJokesType = changeJokesType;
 
 document.getElementById('app-root').innerHTML = App(dataStore);
@@ -25,42 +28,68 @@ document.getElementById('app-root').innerHTML = App(dataStore);
 function App({ setupShown, jokesShown, jokeText, jokesType }) {
   return `
     <div class="${style.app}">
-      <h1 class=${style.app_title}>Good Mood App</h1>
+      <h1 class=${style.appTitle}>Good Mood App</h1>
       ${TypeSwitch()}
-      ${Joke(jokeText, jokesType)}
+      ${JokeBlock(jokeText, jokesType)}
       ${Button(setupShown)}
       ${Counter(jokesShown)}
     </div>
   `;
 }
 
-function Joke(jokeText, jokeType) {
+function LoadIcon() {
   return `
-    <div class="${style.joke} ${jokeType === 'programming' && style.programming}">
-      <p class="${style.joke_text}">${jokeText || 'Click the button to get a joke!'}<span class="${
-    style.joke_underscore
-  }">&nbsp;</span>
-      </p>
+    <img
+      class="${style.loadIcon}"
+      src="${dataStore.jokesType === 'programming' ? progLoadIcon : generalLoadIcon}"
+      alt="loading">
+  `;
+}
+
+function JokeBlock(jokeText, jokeType) {
+  return `
+    <div class="${style.jokeContainer} ${jokeType === 'programming' && style.programming}">
+      ${dataStore.isDataLoading ? LoadIcon() : JokeText(jokeText)}
     </div>`;
+}
+
+function JokeText(jokeText) {
+  return `
+    <p class="${style.jokeText}">${setJokeContent(jokeText)}<span class="${
+    style.jokeUnderscore
+  }">&nbsp;</span></p>
+  `;
+}
+
+function setJokeContent(jokeText) {
+  let content = jokeText || 'Click the button to get a joke!';
+
+  if (dataStore.error) {
+    content = dataStore.error;
+  }
+
+  if (isNewJokeLoaded()) {
+    content = jokeText;
+  }
+
+  return content;
 }
 
 function Button(setup) {
   return setup
-    ? `<button class="${style.app_button}" onclick=showPunchLine()>Punchline</button>`
-    : `<button class="${style.app_button}" onclick=showRandomJoke()>Get a joke</button>`;
+    ? `<button class="${style.appButton}" onclick=setJokePunchLine()>Punchline</button>`
+    : `<button class="${style.appButton}" onclick=setNewRandomJoke()>Get a joke</button>`;
 }
 
 function Counter(jokesShown) {
-  return `<p>Already read jokes: ${jokesShown}`;
+  return `<p>Already read jokes: ${jokesShown}</p>`;
 }
 
 function TypeSwitch() {
-  return `
-    <p>Choose jokes category:</p>
-    ${['general', 'programming']
-      .map(
-        type =>
-          `<label>
+  const typeInputComponents = ['general', 'programming']
+    .map(
+      type =>
+        `<label>
             <input
             id='${type}'
             type='radio'
@@ -70,29 +99,55 @@ function TypeSwitch() {
             ${type}
           </label>
         `,
-      )
-      .join('')}
+    )
+    .join('');
+
+  return `
+    <p>Choose jokes category:</p>
+    ${typeInputComponents}
   `;
+}
+
+async function getRandomJoke(jokeType) {
+  const url = getRandomJokeUrl(jokeType);
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    dataStore.error = error;
+  }
+}
+
+function isNewJokeLoaded() {
+  return dataStore.jokeText && dataStore.currentJoke.punchline !== dataStore.jokeText;
+}
+
+function getRandomJokeUrl(jokeType) {
+  return `https://official-joke-api.appspot.com/jokes/${jokeType}/random`;
 }
 
 function changeJokesType(inputValue) {
   dataStore.jokesType = inputValue;
 }
 
-function showRandomJoke() {
+async function setNewRandomJoke() {
+  dataStore.isDataLoading = true;
   let newJoke;
   while (!newJoke || dataStore.shownIds.includes(newJoke.id)) {
-    newJoke = getRandomJoke(dataStore.jokesType);
+    renderApp();
+    [newJoke] = await getRandomJoke(dataStore.jokesType);
   }
   dataStore.shownIds = [...dataStore.shownIds, newJoke.id];
   dataStore.currentJoke = newJoke;
   dataStore.jokeText = dataStore.currentJoke.setup;
   dataStore.setupShown = true;
+  dataStore.isDataLoading = false;
   dataStore.jokesShown++;
   renderApp();
 }
 
-function showPunchLine() {
+function setJokePunchLine() {
   dataStore.jokeText = dataStore.currentJoke.punchline;
   dataStore.setupShown = false;
   renderApp();
